63d1c32658f571e98acf5342a76f310d5eb119aa
Vaishnav-192 <76587671+vaishnav-192@users.noreply.github.com>
2023-07-14 12:28:33 +0530

    defined what to do

--- contracts/PayPal.sol
+++ contracts/PayPal.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.9;
 
 contract Paypal{
-    
+    //Define the owewr of the smart contract
+
+    //create struct and mapping for request, trans and name
+
+    //add a name to wallet to address
+
+    //create req
+
+    //pay a req
+
+    //get all req sent to usr
+
+    //get all history trans usr has been apart of
 }
